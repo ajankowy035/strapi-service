a9049bd96e9569d0fec6cad2f00785a48373cac7
agnieszka.jankowy <agnieszka.jankowy@espeo.eu>
2023-02-14 15:45:26 +0100

    add schemas for database

---
+++ src/api/bin/controllers/bin.ts
@@ -0,0 +1,7 @@
+/**
+ * bin controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::bin.bin');
---
+++ src/api/garbage-collection-date/controllers/garbage-collection-date.ts
@@ -0,0 +1,7 @@
+/**
+ * garbage-collection-date controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::garbage-collection-date.garbage-collection-date');
